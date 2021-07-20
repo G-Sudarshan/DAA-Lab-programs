a717b5fd13e6b1567916047cb8cd930a73db8635
G Suadrshan <11sudarshang@gmail.com>
2021-07-20 13:16:59 +0530

    practice programs added

---
+++ max_among_3.py
@@ -0,0 +1,12 @@
+# program to take 3 no. as input and print max from them
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+
+if (a > b) and (a > c):
+  print("a is largest")  
+elif (b >= a) and (b >= c):
+  print("b is largest")  
+else:
+  print("c is largest")
